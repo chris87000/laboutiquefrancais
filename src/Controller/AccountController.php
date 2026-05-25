922cff50e282383d624fd272a36e0249c0a8b771
chris87000 <christian.onweb87@gmail.com>
2026-05-25 23:40:42 +0200

    dev en cours...

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,5 +15,21 @@ final class AccountController extends AbstractController
     public function index(): Response
     {
         return $this->render('account/index.html.twig');
+    }
+
+    #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modifiy-pwd')]
+    public function password(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(PasswordUserType::class,$user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            dd($form->getData());
+        }
+        return $this->render('account/password.html.twig',[
+            'modifyPwd' => $form->createView(),
+        ]);
     }
 }
